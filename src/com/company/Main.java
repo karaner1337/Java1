5751b4c1dcfe109988d007c24330061405d7a517
Grzegorz <prove1337@wp.pl>
2020-03-03 15:46:50 +0100

    first comment

---
+++ src/com/company/Main.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Human me = new Human();
+        me.firstName = "Grzegorz";
+        me.lastName = "Lewandowski";
+        lololo dog = new lololo(  "dog");
+        dog.name = "max";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
